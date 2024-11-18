0dc1dc39df7759127271eac3b12c5891227c5df0
Victor Martins <victorgnr2010@hotmail.com>
2024-11-18 18:53:16 -0300

    update

--- templates/index.php
+++ templates/index.php
@@ -1,11 +1,11 @@
 <?php
 
-    include_once('config/url.php');
+    include_once("../config/url.php");
 
 ?>
 
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -15,7 +15,7 @@
     <!-- FONT AWESOME -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css" integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <!-- CSS -->
-     <link rel="stylesheet" href="<?= $BASE_URL?>css/styles.css">
+    <link rel="stylesheet" href="<?= $BASE_URL; ?>/css/styles.css">
 </head>
 <body>
     <h1>Testando agenda</h1>
